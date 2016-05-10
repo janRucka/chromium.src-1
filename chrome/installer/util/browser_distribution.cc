9a7ab2ecc83c9196cf12aad9a88927109d920702
Jan Rucka <Jan.Rucka@firma.seznam.cz>
2016-05-10 17:54:29 +0200

    Seznam.cz icon and Seznam.cz App id instead nwjs (changes in content\nw as well).

--- chrome/installer/util/browser_distribution.cc
+++ chrome/installer/util/browser_distribution.cc
@@ -151,7 +151,7 @@ base::string16 BrowserDistribution::GetActiveSetupGuid() {
 }
 
 base::string16 BrowserDistribution::GetBaseAppName() {
-  return L"nwjs";
+  return L"Seznam.cz";
 }
 
 base::string16 BrowserDistribution::GetDisplayName() {
@@ -192,7 +192,7 @@ base::string16 BrowserDistribution::GetStartMenuShortcutSubfolder(
 }
 
 base::string16 BrowserDistribution::GetBaseAppId() {
-  return L"nwjs";
+  return L"Seznam.cz";
 }
 
 base::string16 BrowserDistribution::GetBrowserProgIdPrefix() {
@@ -208,11 +208,11 @@ base::string16 BrowserDistribution::GetBrowserProgIdDesc() {
 
 
 base::string16 BrowserDistribution::GetInstallSubDir() {
-  return L"nwjs";
+  return L"Seznam.cz";
 }
 
 base::string16 BrowserDistribution::GetPublisherName() {
-  return L"nwjs";
+  return L"Seznam.cz";
 }
 
 base::string16 BrowserDistribution::GetAppDescription() {
@@ -226,7 +226,7 @@ base::string16 BrowserDistribution::GetLongAppDescription() {
 }
 
 std::string BrowserDistribution::GetSafeBrowsingName() {
-  return "nwjs";
+  return "Seznam.cz";
 }
 
 base::string16 BrowserDistribution::GetDistributionData(HKEY root_key) {
