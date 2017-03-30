5b9477134e11068244ae1f7d841ba1cb72f968ce
Jan Rucka <Jan.Rucka@firma.seznam.cz>
2017-03-30 10:10:43 +0200

    Browser registration hotfix for MacOS and linux.

--- chrome/browser/shell_integration_linux.cc
+++ chrome/browser/shell_integration_linux.cc
@@ -234,6 +234,11 @@ bool SetAsDefaultBrowser() {
   return SetDefaultWebClient(std::string());
 }
 
+bool Register(base::Callback<void(bool)> callback) {
+  callback.Run(true);
+  return true;
+}
+
 bool SetAsDefaultProtocolClient(const std::string& protocol) {
   return SetDefaultWebClient(protocol);
 }
