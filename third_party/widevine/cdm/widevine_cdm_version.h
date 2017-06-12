51739004ab83cf109d1b4ccfcd474e3a7176eda6
Miloslav Mrvík <miloslav.mrvik@firma.seznam.cz>
2017-06-12 15:42:03 +0200

    Změny pro MAC

--- third_party/widevine/cdm/widevine_cdm_version.h
+++ third_party/widevine/cdm/widevine_cdm_version.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,10 +8,8 @@
 #ifndef WIDEVINE_CDM_VERSION_H_
 #define WIDEVINE_CDM_VERSION_H_
 
-// If the Widevine CDM were available, we would include
-// third_party/widevine/cdm/widevine_cdm_common.h and define two things:
-//  - WIDEVINE_CDM_AVAILABLE (to indicate availability), and
-//  - WIDEVINE_CDM_VERSION_STRING (with the version of the CDM that's available
-//    as a string, e.g., "1.0.123.456").
+#include "third_party/widevine/cdm/widevine_cdm_common.h"
+
+#define WIDEVINE_CDM_AVAILABLE
 
 #endif  // WIDEVINE_CDM_VERSION_H_
