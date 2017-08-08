d147bdb53b78b42e0c7ed7a12c053dc1d70c22a3
Miloslav Mrvík <miloslav.mrvik@firma.seznam.cz>
2017-08-08 16:50:15 +0200

    Changes to compile on Linux

--- base/values.cc
+++ base/values.cc
@@ -1261,12 +1261,10 @@ void ListValue::Append(std::unique_ptr<Value> in_value) {
   list_->push_back(std::move(in_value));
 }
 
-#if !defined(OS_LINUX)
 void ListValue::Append(Value* in_value) {
   DCHECK(in_value);
   Append(WrapUnique(in_value));
 }
-#endif
 
 void ListValue::AppendBoolean(bool in_value) {
   Append(MakeUnique<Value>(in_value));
