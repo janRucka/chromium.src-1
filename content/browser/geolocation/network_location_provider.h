7a9a23f6efc513ec7dad672aae54b837fb7d3b1a
Jan Rucka <ruckajan10@gmail.com>
2017-10-17 10:46:04 +0200

    geolocation

--- content/browser/geolocation/network_location_provider.h
+++ content/browser/geolocation/network_location_provider.h
@@ -15,6 +15,7 @@
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/strings/string16.h"
+#include "base/time/time.h"
 #include "base/threading/non_thread_safe.h"
 #include "base/threading/thread.h"
 #include "content/browser/geolocation/location_provider_base.h"
@@ -36,6 +37,7 @@ class NetworkLocationProvider
    public:
     // The maximum size of the cache of positions.
     static const size_t kMaximumSize;
+    static const base::TimeDelta kCacheExpiration;
 
     PositionCache();
     ~PositionCache();
@@ -52,19 +54,13 @@ class NetworkLocationProvider
     // position if available. Ownership remains with the cache.
     const Geoposition* FindPosition(const WifiData& wifi_data);
 
-   private:
-    // Makes the key for the map of cached positions, using a set of
-    // data. Returns true if a good key was generated, false otherwise.
-    static bool MakeKey(const WifiData& wifi_data,
-                        base::string16* key);
+ private:
+    // Check whether cache is full or has outdated entries
+    void CacheChecker();
 
     // The cache of positions. This is stored as a map keyed on a string that
-    // represents a set of data, and a list to provide
-    // least-recently-added eviction.
-    typedef std::map<base::string16, Geoposition> CacheMap;
-    CacheMap cache_;
-    typedef std::list<CacheMap::iterator> CacheAgeList;
-    CacheAgeList cache_age_list_;  // Oldest first.
+    // represents a set of data
+    std::map<base::string16, Geoposition> cache_;
   };
 
   NetworkLocationProvider(AccessTokenStore* access_token_store,
