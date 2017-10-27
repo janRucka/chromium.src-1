727aa12aee866a23a7b5c512361977f14b457767
Jan Rucka <ruckajan10@gmail.com>
2017-10-27 10:34:23 +0200

    Register browser. It is possible to register via "nw.App.registerBrowser" function (changes in chromium project '\src\content\nw' as well).

--- chrome/browser/shell_integration_win.cc
+++ chrome/browser/shell_integration_win.cc
@@ -569,6 +569,22 @@ bool SetAsDefaultProtocolClient(const std::string& protocol) {
   return true;
 }
 
+bool Register(base::Callback<void(bool)> callback)
+{
+  base::FilePath chrome_exe;
+  if (!PathService::Get(base::FILE_EXE, &chrome_exe)) {
+    LOG(ERROR) << "Error getting app exe path";
+    callback.Run(false);
+    return false;
+  }
+
+  BrowserDistribution* dist = BrowserDistribution::GetDistribution();
+  bool retVal = ShellUtil::RegisterChromeBrowser(dist, chrome_exe, base::string16(), true);
+
+  callback.Run(retVal);
+  return retVal;
+}
+
 DefaultWebClientSetPermission GetDefaultWebClientSetPermission() {
   if (!install_static::SupportsSetAsDefaultBrowser())
     return SET_DEFAULT_NOT_ALLOWED;
